1fbb3c6c9a0b5a7515fbbae801b9464b41c9c05e
pesonB <personB@iru.university>
2022-09-18 23:20:10 +0300

    create(class person & propiretise)

---
+++ fun.java
@@ -0,0 +1,12 @@
+public class fun{
+    public static void main(String[] args) {
+        System.out.println("hallo");
+    }
+}
+class Person{
+    private int id;
+    private String name;
+    private String email_adress;
+    private String phone_number;
+    
+}
